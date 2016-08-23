1776fe5db56ba22c5765f20a0192cc6b83cc8d0b
ClassRoom_001 <arkim@cookapps.com>
2016-08-23 18:48:25 +0900

    부엌 리소스 추가, 고양이 움직움직

---
+++ IAmHungry/Assets/Cat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat : MonoBehaviour
+{
+	public float moveSpeed = 5;
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position = transform.position + new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position = transform.position + new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+		}
+	}
+}
