c49b856c5db6aa7a008dc22e752bd42a21613e57
ClassRoom_001 <arkim@cookapps.com>
2016-08-24 14:57:21 +0900

    고양이 점프점프

--- IAmHungry/Assets/Cat.cs
+++ IAmHungry/Assets/Cat.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class Cat : MonoBehaviour
 {
 	public float moveSpeed = 5;
+	public float jumpSpeed = 100;
+	public Rigidbody2D rigidbody;
+
 	void Update()
 	{
 		if(Input.GetKey(KeyCode.RightArrow))
 		{
 			transform.position = transform.position + new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+			transform.eulerAngles = new Vector3 (0, 0, 0);
 		}
 		if(Input.GetKey(KeyCode.LeftArrow))
 		{
 			transform.position = transform.position + new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+			transform.eulerAngles = new Vector3 (0, 180, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody.AddForce (new Vector2(0, jumpSpeed * Time.deltaTime), ForceMode2D.Impulse);
 		}
 	}
 }
